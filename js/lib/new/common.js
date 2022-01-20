25eb1b510c7000ed99b6a4e9e3383ac06c72c1de
vasylzayachkivskyi <vasylzaya@gmail.com>
2022-01-20 16:57:00 +0200

    fix

--- js/lib/new/common.js
+++ js/lib/new/common.js
@@ -1,7 +1,72 @@
-if ($(".phoneinp").length) {
-    $('.phoneinp').inputmask({
-        "mask": "99 /999/ 9999999",
-        showMaskOnHover: false,
-        showMaskOnFocus: false,
+$(document).ready(function () {
+    // input mask
+    if ($(".phoneinp").length) {
+        $('.phoneinp').inputmask({
+            "mask": "99 /999/ 9999999",
+            showMaskOnHover: false,
+            showMaskOnFocus: false,
+        });
+    }
+
+    // slider
+
+    $('.universities__litslider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                },
+            },
+        ],
     });
-}
+
+    // accordeon
+    $('.specialization__header').click(function () {
+        $(this).toggleClass('open');
+        $(this).next('.specialization__body').slideToggle();
+    });
+
+    $('.faq__header').click(function () {
+        $(this).toggleClass('open');
+        $(this).next('.faq__body').slideToggle();
+    });
+
+    // ---
+    $('.arrowtoggle').click(function () {
+        $(this).prev().slideToggle();
+        $(this).toggleClass('rotate');
+    });
+
+    // datepicker
+    if ($('body *').is('.datepicker')) {
+        $('.datepicker').datepicker({
+            autoclose: true,
+            todayHighlight: true,
+            orientation: 'bottom',
+            format: 'dd/mm/yyyy',
+        });
+    }
+    // form styler
+    if ($('body *').is('select')) {
+        $(function () {
+            $('input, select').styler();
+        });
+    }
+
+
+
+
+});
